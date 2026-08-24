064fc2a66f26e184b9661698614b3a233bdbd1dc
Ali <alihossen01646@gmail.com>
2026-08-24 14:19:56 +0600

    New projct init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Show menu */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Hide menu */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
