d054d3dfa512a9a89a22a6f993fa1b2f5962738b
Ali <alihossen01646@gmail.com>
2026-08-25 16:58:04 +0600

    home section just completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,33 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+const showMenu = (navId, toggleId) => {
+   const nav = document.getElementById(navId),
+         toggle = document.getElementById(toggleId)
 
-/* Show menu */
-if(navToggle){
-   navToggle.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu')
+   toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu')
    })
 }
-
-/* Hide menu */
-if(navClose){
-   navClose.addEventListener('click', () =>{
-      navMenu.classList.remove('show-menu')
-   })
-}
+showMenu('nav-menu', 'nav-toggle')
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+
+const linkAction = () =>{
+   const navMenu = document.getElementById('nav-menu')
+   // When we click on each nav__link, we remove the show-menu class
+   navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE HEADER STYLES ===============*/
+const scrollHeader = () =>{
+   const header = document.getElementById('header')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+   this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                      : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== ADD SHADOW HEADER ===============*/
